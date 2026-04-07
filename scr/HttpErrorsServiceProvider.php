44638fda7ab204e4726314ae77ea1cafc029a17e
anthonymacs <157227172+anthonymacs@users.noreply.github.com>
2026-04-07 13:37:21 +0700

    fix vendor name to anthonymacs

--- scr/HttpErrorsServiceProvider.php
+++ scr/HttpErrorsServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Yourvendor\HttpErrors;
+namespace Anthonymacs\HttpErrors;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -8,10 +8,8 @@ class HttpErrorsServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        // Load the package's error views under the "errors" namespace
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'errors');
 
-        // Allow publishing views to the host app for customization
         $this->publishes([
             __DIR__ . '/../resources/views/errors' => resource_path('views/errors'),
         ], 'http-errors-views');
